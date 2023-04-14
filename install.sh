889fef88a9ecd0aa8eaf4659362f4de1be0202ef
arnavgr <arnavgr@hotmail.com>
2023-04-14 14:30:51 +0530

    updated/added stuff

---
+++ install.sh
@@ -0,0 +1,17 @@
+# This install all packages along with aur
+sudo pacman -Syu --needed base-devel nvim w3m github-cli neofetch firefox ranger zsh lxappearance htop feh pamixer flatpak ueberzug atool poppler imagemagick highlight zathura-pdf-poppler
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd -
+yay -S ttf-firacode-nerd checkupdates-with-aur 
+
+# This is used to symlink config files to .config
+ln -s "$PWD/.config/zsh/" ~/.config
+ln -s "$PWD/.zshenv" ~
+ln -s "$PWD/.config/nvim" ~/.config
+ln -s "$PWD/.config/ranger" ~/.config
+ln -s "$PWD/.config/zathura" ~/.config
+
+
+
