8bb809ea5a6cc86b42ea366eeb487dd9e82a6e9f
arnavgr <arnavgr@hotmail.com>
2023-04-20 00:17:00 +0530

    updated/added stuff

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 # This install all packages along with aur
-sudo pacman -Syu --needed base-devel nvim w3m github-cli neofetch firefox ranger zsh htop feh pamixer flatpak ueberzug atool poppler imagemagick highlight zathura-pdf-poppler brightnessctl dunst maim xclip picom pacman-contrib xdotool acpi gvim mpv ncmpcpp
+sudo pacman -Syu --needed base-devel nvim w3m github-cli neofetch firefox ranger zsh htop feh pamixer flatpak ueberzug atool poppler imagemagick highlight zathura-pdf-poppler brightnessctl dunst maim xclip picom pacman-contrib xdotool acpi gvim mpv ncmpcpp libxcb bluez bluez-utils
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd -
-yay -S ttf-firacode-nerd  
+yay -S ttf-firacode-nerd udiskie networkmanager-dmenu-git 
 
 # This is used to symlink config files to .config
-ln -s "$PWD/.config/zsh/" ~/.config
+ln -s "$PWD/.config/zsh/" $HOME/.config
 ln -s "$PWD/.zshenv" $HOME
-ln -s "$PWD/.config/nvim" ~/.config
-ln -s "$PWD/.config/ranger" ~/.config
-ln -s "$PWD/.config/zathura" ~/.config
-ln -s "$PWD/.config/picom" ~/.config
-ln -s "$PWD/.config/dunst" ~/.config
-ln -s "$PWD/.config/neofetch" ~/.config
+ln -s "$PWD/.config/nvim" $HOME/.config
+ln -s "$PWD/.config/ranger" $HOME/.config
+ln -s "$PWD/.config/zathura" $HOME/.config
+ln -s "$PWD/.config/picom" $HOME/.config
+ln -s "$PWD/.config/dunst" $HOME/.config
+ln -s "$PWD/.config/neofetch" $HOME/.config
 ln -s "$PWD/bin" $HOME
 ln -s "$PWD/.xintrc" $HOME
 ln -s "$PWD/X11" $HOME
+ln -s /home/arnav/dotfiles/.config/networkmanager-dmenu/ $HOME/.config
 
 
 
