c6543b9aa08a2b0c0959a76a7fa86a4037478387
arnavgr <arnavgr@hotmail.com>
2023-12-10 22:29:39 +0530

    updated/added stuff

--- termux-install.sh
+++ termux-install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-apt update && apt upgrade
+#apt update && apt upgrade
 
-pkg autoclean
-pkg clean
+#pkg autoclean
+#pkg clean
 
-termux-setup-storage
+#termux-setup-storage
 
-apt install curl wget git neovim python grep htop openssh zsh gh neofetch
+#apt install curl wget git neovim python grep htop openssh zsh gh neofetch ranger
 
 ln -sf "$PWD/.config/zsh/" $HOME/.config
 
 ln -sf "$PWD/.config/nvim/" $HOME/.config
+
+ln -sf "$PWD/.config/ranger/" $HOME/.config
+
+ln -sf "$PWD/.config/neofetch/" $HOME/.config
+
+ln -sf "$PWD/.zshenv/" $HOME/
+
+chsh
