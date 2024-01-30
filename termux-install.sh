c7b4484873a19104dc3e81bdfbf61b349597b4dd
arnavgr <103447227+arnavgr@users.noreply.github.com>
2024-01-30 14:25:36 +0530

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-apt update && apt upgrade
+pkg upgrade
 
 pkg autoclean
 pkg clean
 
 termux-setup-storage
 
-apt install curl wget git neovim python grep htop openssh zsh gh neofetch ranger lsd
+pkg install curl wget git neovim python grep htop openssh zsh gh neofetch ranger lsd
+
+curl -s https://raw.githubusercontent.com/nohajc/termux-adb/master/install.sh | bash
 
 ln -sf "$PWD/.config/zsh/" $HOME/.config
 
