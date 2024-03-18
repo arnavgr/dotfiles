3a1495460a1b797b9602e12ba3d7f82e8b432448
arnavgr <arnavgr@hotmail.com>
2024-03-19 00:08:27 +0530

    added stuff and rewriting the scripts

--- termux-install.sh
+++ termux-install.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
+# Upgrade installed packages
 pkg upgrade
 
+# Clean up cached package files
 pkg autoclean
 pkg clean
 
+# Grant storage permissions
 termux-setup-storage
 
-pkg install curl wget git neovim python grep htop openssh zsh gh neofetch ranger lsd
+# Install essential packages
+pkg install curl wget git neovim python grep htop openssh zsh gh neofetch
 
+# Install Termux ADB
 curl -s https://raw.githubusercontent.com/nohajc/termux-adb/master/install.sh | bash
 
+# Create necessary directories
 mkdir -p $HOME/.config
 
+# Create symbolic links for configuration files
+ln -sf "$PWD/bin/" $HOME/.local
 ln -sf "$PWD/.config/zsh/" $HOME/.config
-
 ln -sf "$PWD/.config/nvim/" $HOME/.config
-
 ln -sf "$PWD/.config/ranger/" $HOME/.config
-
 ln -sf "$PWD/.config/neofetch/" $HOME/.config
 
-ln -sf "$PWD/.zshenv/" $HOME/
+# Copy zsh environment configuration
+cp .zshenv $HOME
 
+# Change default shell to zsh
 chsh -s zsh
+
+# Install Termux Notion
+curl -fsSL https://raw.githubusercontent.com/arnavgr/termux-notion/main/install.sh | bash
+
+# Run getnf command to set up Termux Notion
+getnf
+
