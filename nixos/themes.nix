f636e3897037ed6322906e585a362c80ae87c77c
arnavgr <arnavgr@hotmail.com>
2025-06-22 11:51:50 +0530

    updated/added stuff

--- nixos/themes.nix
+++ nixos/themes.nix
@@ -15,6 +15,10 @@
   services.xserver.displayManager.sessionCommands = ''
     export XCURSOR_THEME=Bibata-Modern-Classic
     export XCURSOR_SIZE=24
+    export GTK_THEME=Dracula
+    export GDK_BACKEND=wayland,x11
+    export FONT_NAME="GohuFont Nerd Font Mono"
+    export FONT_SIZE=10
   '';
 
   # Environment variables for GTK/Wayland apps
@@ -23,6 +27,8 @@
     XCURSOR_THEME = "Bibata-Modern-Classic";
     XCURSOR_SIZE = "24";
     GDK_BACKEND = "wayland,x11";
+    FONT_NAME = "GohuFont Nerd Font Mono";
+    FONT_SIZE = "10";
   };
 }
 
